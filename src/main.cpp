3741fd9c0eec113dcfd003f1a1c27d7a7462c175
Eduardo <gualberto.se@ymail.com>
2019-12-13 13:10:54 -0300

    tela inicial em desenvolvimento

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,75 @@ using namespace sf;
 
 */
 
+void startMenu(RenderWindow* window){
+    
+    Font font;
+    font.loadFromFile("font_start_menu.ttf");
+    Text text;
+    text.setFont(font);
+    text.setString("PRESS ANY KEY TO START"); 
+    text.setPosition(235, 272.5);
+    text.setCharacterSize(30);
+    float text_alpha = 255;
+
+    Texture backgorund_img;
+    backgorund_img.loadFromFile("img/space_bg.jpg");
+
+    Sprite backgorund(backgorund_img);
+    backgorund.setScale(.475, .7);
+
+    Texture backgorund_txt_img;
+    backgorund_txt_img.loadFromFile("img/start_menu_txt_bg.png");
+
+    Sprite backgorund_txt(backgorund_txt_img);
+    backgorund_txt.setScale(12.5, 4.5);
+    backgorund_txt.setPosition(160, 255);
+    
+    while (window->isOpen())
+    {
+        Event e;
+        while (window->pollEvent(e))
+        {
+            if(e.type == Event::Closed)
+                window->close();
+            if(e.type == Event::KeyPressed)
+                return;
+        }
+
+
+        text.setFillColor(Color(255,255,255,text_alpha-=3));
+        if(text_alpha <= 70)
+            text_alpha = 255;
+        window->clear(Color(255,255,255));
+
+        window->draw(backgorund);
+        window->draw(backgorund_txt);
+        window->draw(text);
+
+        window->display();
+    }
+    
+}
+
+
 int main(){
 
-    RenderWindow window(VideoMode(500, 500), "Teste!");
+    RenderWindow window(VideoMode(900, 600), "Teste!");
+    window.setFramerateLimit(60);
+
+    Texture backgorund_img;
+    backgorund_img.loadFromFile("img/space_bg.jpg");
+
+    Sprite backgorund(backgorund_img);
+    backgorund.setScale(.475, .7);
 
     Laser laser1(ALIADO, 400, 400, 2, 0);
     Laser laser2(INIMIGO, 350, 350, -2, -2);
 
     CircleShape circle(100.f);
-    circle.setFillColor(Color(0,0,0));
+    circle.setFillColor(Color(255,255,255));
+
+    startMenu(&window);
 
     while (window.isOpen())
     {
@@ -41,6 +101,7 @@ int main(){
         }
         
         window.clear(Color(255,255,255));
+        window.draw(backgorund);
 
         window.draw(circle);
         window.draw(laser1);
