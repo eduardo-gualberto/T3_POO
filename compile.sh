ab450b1388b2d94d9fcb24ab601f723f0340da0d
victorlx <victor.antunes.2000@gmail.com>
2019-12-14 14:23:35 -0300

    parametro para compilar biblioteca de audio

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-g++ -c src/*.cpp
-g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c src/*.cpp -Wall
+g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 
 ./sfml-app
