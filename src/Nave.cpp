148e705042cd2eb6d5f69c6308d3c487117b2caa
LVD2304 <52726171+LVD2304@users.noreply.github.com>
2019-12-15 14:07:16 -0300

    Update Nave.cpp

--- src/Nave.cpp
+++ src/Nave.cpp
@@ -8,19 +8,43 @@ Nave::Nave(){
     this->setTexture(*texture);
 }
 void Nave::update(int dir){
+    Vector2f pos = this->getPosition();
+    int px = pos.x;
+    int py = pos.y;
     switch(dir){
         case(1): //Eixo y para cima
-            this->move(0,-20);
-            break;
+            if(py >= 0){
+                this->move(0,-10);
+                break;
+            }else{
+                this->move(0,0);
+                break;
+            }
         case (2):
-            this->move(0,20);  //Eixo y para baixo;
-            break;
+            if(py<=550){
+                this->move(0,10);  //Eixo y para baixo;
+                break;
+            }else{
+                this->move(0,0);
+                break;
+            }
         case(3):
-            this->move(20,0); //Eixo x para a direita;
-            break;
+            if(px<=850){
+                this->move(10,0); //Eixo x para a direita;
+                break;
+            }else{
+                this->move(0,0);
+                break;
+            }
         case(4):
-            this->move(-20,0); //Eixo y para a esquerda;
-            break;
+            if(px>=0){
+                this->move(-10,0); //Eixo x para a esquerda;
+                break;
+            }else{
+                this->move(0,0);
+                break;
+            }
+            
     }
 
 }
