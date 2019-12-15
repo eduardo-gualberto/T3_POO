2a9395b6046803ca43719e3ffce82da17b67d14a
victorlx <victor.antunes.2000@gmail.com>
2019-12-15 16:47:25 -0300

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "NaveInimiga.h"
 #include "Item.h"
 #include "Nave.hpp"
-#include <sstream>  //para imprimir coisas na tela
+#include <sstream> //para imprimir coisas na tela
 #include <iostream>
 using namespace sf;
 
@@ -20,21 +20,29 @@ using namespace sf;
 
 
 */
+/*
+SoundBuffer SetSom(string nomeArquivo)
+{
+    SoundBuffer nomeBuffer;
+    if (!nomeBuffer.loadFromFile(nomeArquivo))
+        std::cout << "Não foi possível abrir arquivo de áudio" << std::endl;
+    return nomeBuffer;
+}
+*/
+void startMenu(RenderWindow *);
+void mainGame(RenderWindow *);
+void endMenu(RenderWindow *);
 
-void startMenu(RenderWindow*);
-void mainGame(RenderWindow*);
-void endMenu(RenderWindow*);
+void startMenu(RenderWindow *window)
+{
 
-
-void startMenu(RenderWindow* window){
-    
     bool transition = false;
 
     Font font;
     font.loadFromFile("font_start_menu.ttf");
     Text text;
     text.setFont(font);
-    text.setString("PRESS ANY KEY TO START"); 
+    text.setString("PRESS ANY KEY TO START");
     text.setPosition(235, 272.5);
     text.setCharacterSize(30);
     float text_alpha = 255;
@@ -59,34 +67,34 @@ void startMenu(RenderWindow* window){
         std::cout << "Não foi possível abrir arquivo de áudio" << std::endl;
     Sound som;
     som.setBuffer(buffer);
-    
+
     while (window->isOpen())
     {
         Event e;
         while (window->pollEvent(e))
         {
-            if(e.type == Event::Closed)
+            if (e.type == Event::Closed)
                 window->close();
-            if(e.type == Event::KeyPressed){
+            if (e.type == Event::KeyPressed)
+            {
                 transition = true;
                 dalpha_bg_txt = 3;
                 text_alpha = bg_txt_alpha = 255;
                 som.setVolume(50);
                 som.play();
             }
-            
         }
 
-
-        text.setFillColor(Color(255,255,255,text_alpha-=3));
-        backgorund_txt.setColor(Color(255,255,255,bg_txt_alpha-=dalpha_bg_txt));
-        if(!transition && text_alpha <= 70)
+        text.setFillColor(Color(255, 255, 255, text_alpha -= 3));
+        backgorund_txt.setColor(Color(255, 255, 255, bg_txt_alpha -= dalpha_bg_txt));
+        if (!transition && text_alpha <= 70)
             text_alpha = 255;
-        if(transition && text_alpha <= 10){
+        if (transition && text_alpha <= 10)
+        {
             mainGame(window);
             return;
         }
-        window->clear(Color(255,255,255));
+        window->clear(Color(255, 255, 255));
 
         window->draw(backgorund);
         window->draw(backgorund_txt);
@@ -94,25 +102,25 @@ void startMenu(RenderWindow* window){
 
         window->display();
     }
-    
 }
 
-void endMenu(RenderWindow* window){
-    
+void endMenu(RenderWindow *window)
+{
+
     bool transition = false;
 
     Font font;
     font.loadFromFile("font_start_menu.ttf");
     Text text;
     text.setFont(font);
-    text.setString("END OF JOURNEY, JEDI"); 
+    text.setString("END OF JOURNEY, JEDI");
     text.setPosition(235, 272.5);
     text.setCharacterSize(30);
     float text2_alpha = 255;
 
     Text text2;
     text2.setFont(font);
-    text2.setString("PRESS ANY KEY TO RESTART"); 
+    text2.setString("PRESS ANY KEY TO RESTART");
     text2.setPosition(235, 307.5);
     text2.setCharacterSize(20);
 
@@ -133,24 +141,23 @@ void endMenu(RenderWindow* window){
         Event e;
         while (window->pollEvent(e))
         {
-            if(e.type == Event::Closed)
+            if (e.type == Event::Closed)
                 window->close();
-            if(e.type == Event::KeyPressed){
+            if (e.type == Event::KeyPressed)
+            {
                 transition = true;
                 text2_alpha = 255;
                 sabre.play();
             }
-            
         }
 
-
-        text2.setFillColor(Color(255,255,255,text2_alpha-=3));
-        if(!transition && text2_alpha <= 70)
+        text2.setFillColor(Color(255, 255, 255, text2_alpha -= 3));
+        if (!transition && text2_alpha <= 70)
             text2_alpha = 255;
-        if(transition && text2_alpha <= 10)
+        if (transition && text2_alpha <= 10)
             return;
 
-        window->clear(Color(255,255,255));
+        window->clear(Color(255, 255, 255));
 
         window->draw(backgorund);
         window->draw(text);
@@ -160,12 +167,12 @@ void endMenu(RenderWindow* window){
     }
 }
 
-
 //janela formada será de LARGURA x ALTURA
-#define LARGURA 900  
+#define LARGURA 900
 #define ALTURA 600
 
-void mainGame(RenderWindow* window){
+void mainGame(RenderWindow *window)
+{
 START:
     Texture backgorund_img;
     backgorund_img.loadFromFile("img/space_bg.jpg");
@@ -192,12 +199,15 @@ START:
     hud.setFont(font);
     hud.setCharacterSize(20);
     hud.setFillColor(sf::Color::White);
+
     //variaveis
     int score = 0;
     int lives = 5;
     int ItemDesativado = 1;
     Clock clk;
 
+    //sons usados no jogo
+    /*                                                                          */
     SoundBuffer buffer_explosao;
     if (!buffer_explosao.loadFromFile("music/jogador_explode.ogg"))
         std::cout << "Não foi possível abrir arquivo de áudio" << std::endl;
@@ -216,25 +226,37 @@ START:
     Sound som_tiro;
     som_tiro.setBuffer(buffer_tiro);
 
+    SoundBuffer buffer_tiroinimigo;
+    if (!buffer_tiroinimigo.loadFromFile("music/inimigo_atira.ogg"))
+        std::cout << "Não foi possível abrir arquivo de áudio" << std::endl;
+    Sound som_tiroinimigo;
+    som_tiroinimigo.setBuffer(buffer_tiroinimigo);
+    /*                                                                          */
+
     while (window->isOpen())
     {
         Event e;
         while (window->pollEvent(e))
         {
-            if(e.type == Event::Closed)
+            if (e.type == Event::Closed)
                 window->close();
         }
-        if (Keyboard::isKeyPressed(Keyboard::W))
+        if ((Keyboard::isKeyPressed(Keyboard::W)) || (Keyboard::isKeyPressed(Keyboard::Up)))
             nave.update(1);
-        if (Keyboard::isKeyPressed(Keyboard::S))
+        if ((Keyboard::isKeyPressed(Keyboard::S)) || (Keyboard::isKeyPressed(Keyboard::Down)))
             nave.update(2);
-        if (Keyboard::isKeyPressed(sf::Keyboard::D))
+        if ((Keyboard::isKeyPressed(Keyboard::D)) || (Keyboard::isKeyPressed(Keyboard::Right)))
             nave.update(3);
-        if (Keyboard::isKeyPressed(sf::Keyboard::A))
+        if ((Keyboard::isKeyPressed(Keyboard::A)) || (Keyboard::isKeyPressed(Keyboard::Left)))
             nave.update(4);
+        /*if ((Keyboard::isKeyPressed(Keyboard::Space)
+
+        */
 
         // Bloco que faz a nave inimiga atiraar
-        if(laserInimigo1.getPosition().y >= ALTURA && laserInimigo2.getPosition().y >= ALTURA && laserInimigo3.getPosition().y >= ALTURA) {
+        if (laserInimigo1.getPosition().y >= ALTURA && laserInimigo2.getPosition().y >= ALTURA && laserInimigo3.getPosition().y >= ALTURA)
+        {
+            som_tiroinimigo.play();
             laserInimigo1.setPosition(inimigo.getPosition().x + inimigo.getTexture()->getSize().x / 2, inimigo.getPosition().y + inimigo.getTexture()->getSize().y);
             laserInimigo2.setPosition(inimigo.getPosition().x + inimigo.getTexture()->getSize().x / 2, inimigo.getPosition().y + inimigo.getTexture()->getSize().y);
             laserInimigo3.setPosition(inimigo.getPosition().x + inimigo.getTexture()->getSize().x / 2, inimigo.getPosition().y + inimigo.getTexture()->getSize().y);
@@ -243,63 +265,107 @@ START:
             laserInimigo3.update();
         }
 
-        if ((inimigo.getGlobalBounds().left > LARGURA - inimigo.getTexture()->getSize().x) || (inimigo.getGlobalBounds().left < 0)){
+        //verifica se o inimigo n vai sair da tela
+        if ((inimigo.getGlobalBounds().left > LARGURA - inimigo.getTexture()->getSize().x) || (inimigo.getGlobalBounds().left < 0))
+        {
             inimigo.SaiuDaTela();
         }
+
+        //verifica se os meteoros ja sairam da tela
         if (meteoro1.getGlobalBounds().top > ALTURA)
         {
             meteoro1.hit(0);
-            score++;        //no lugar vai ser setscore de jogador
+            score++; //no lugar vai ser setscore de jogador
         }
         if (meteoro2.getGlobalBounds().top > ALTURA)
         {
             meteoro2.hit(0);
-            score++;        //no lugar vai ser setscore de jogador
+            score++; //no lugar vai ser setscore de jogador
         }
         if (meteoro3.getGlobalBounds().top > ALTURA)
         {
             meteoro3.hit(0);
-            score++;        //no lugar vai ser setscore de jogador
+            score++; //no lugar vai ser setscore de jogador
         }
         if (meteoroE.getGlobalBounds().top > ALTURA)
         {
             meteoroE.hit(1);
-            score++;        //no lugar vai ser setscore de jogador
+            score++; //no lugar vai ser setscore de jogador
         }
 
-        //meteoro tocou na nave
-        if (meteoro1.getGlobalBounds().intersects(nave.getGlobalBounds())){
-            lives--;        //no lugar vai ser setlife de jogador
+        //verifca se o inimigo tocou na nave
+        if (inimigo.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
+            lives--;
+            //implementar morte da nave inimiga
+        }
+
+        //verifca se os lasers tocaram na nave
+        if (laserInimigo1.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
+            lives--; //no lugar vai ser setlife de jogador
+            explosao.play();
+            laserInimigo1.setPosition(10000, 10000);
+        }
+        if (laserInimigo2.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
+            lives--; //no lugar vai ser setlife de jogador
+            explosao.play();
+            laserInimigo2.setPosition(10000, 10000);
+        }
+        if (laserInimigo3.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
+            lives--; //no lugar vai ser setlife de jogador
+            explosao.play();
+            laserInimigo3.setPosition(10000, 10000);
+        }
+
+        //verifica se os meteoros tocaram na nave
+        if (meteoro1.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
+            lives--; //no lugar vai ser setlife de jogador
             explosao.play();
             meteoro1.hit(0);
         }
-        if (meteoro2.getGlobalBounds().intersects(nave.getGlobalBounds())){
-            lives--;        //no lugar vai ser setlife de jogador
+        if (meteoro2.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
+            lives--; //no lugar vai ser setlife de jogador
             explosao.play();
             meteoro2.hit(0);
         }
-        if (meteoro3.getGlobalBounds().intersects(nave.getGlobalBounds())){
-            lives--;        //no lugar vai ser setlife de jogador
+        if (meteoro3.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
+            lives--; //no lugar vai ser setlife de jogador
             explosao.play();
             meteoro3.hit(0);
         }
-        if (meteoroE.getGlobalBounds().intersects(nave.getGlobalBounds())){
-            lives = lives - 3;//no lugar vai ser setlife de jogador
+        if (meteoroE.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
+            lives = lives - 3; //no lugar vai ser setlife de jogador
             explosao.play();
             meteoroE.hit(1);
         }
+
+        //contador para o item vida
         Time tempoPassado = clk.getElapsedTime();
-        if (int(tempoPassado.asSeconds()) > 10){    //nova vida aparecera a cada 10 segundos
+        if (int(tempoPassado.asSeconds()) > 10)
+        { //nova vida aparecera a cada 10 segundos
             clk.restart();
             ItemDesativado *= -1;
             vida.ItemAction(ItemDesativado);
         }
-        if (vida.getGlobalBounds().intersects(nave.getGlobalBounds())){
+
+        //verifica se o jogador tocou na vida
+        if (vida.getGlobalBounds().intersects(nave.getGlobalBounds()))
+        {
             som_vida.play();
-            lives++;        //no lugar vai ser setlife de jogador
+            lives++; //no lugar vai ser setlife de jogador
             vida.ItemCatch();
         }
-        if(lives <= 0){
+
+        //se as vidas acabaram o jogo termina
+        if (lives <= 0)
+        {
             explosao.play();
             endMenu(window);
             goto START;
@@ -316,22 +382,13 @@ START:
         laserInimigo2.update();
         laserInimigo3.update();
 
-
-        if(laser1.getPosition().x > LARGURA){
-            som_tiro.play();
-            laser1.setPosition(0, ALTURA/2);
-        }
-        if(laser1.getPosition().y > ALTURA){
-            som_tiro.play();
-            laser1.setPosition(LARGURA, 0);
-        }
-
         //imprimindo hud
         std::stringstream ss;
-        ss << " -> Score:" << score << std::endl << " -> Lives:" << lives << std::endl;
+        ss << " -> Score:" << score << std::endl
+           << " -> Lives:" << lives << std::endl;
         hud.setString(ss.str());
-        
-        window->clear(Color(255,255,255,255));
+
+        window->clear(Color(255, 255, 255, 255));
         window->draw(backgorund);
         window->draw(laser1);
         window->draw(laserInimigo1);
@@ -350,7 +407,8 @@ START:
     }
 }
 
-int main(){
+int main()
+{
 
     RenderWindow window(VideoMode(LARGURA, ALTURA), "Teste!");
     window.setFramerateLimit(60);
